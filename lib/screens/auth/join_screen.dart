7bfe75638ec62f7f68b18e511708b458b44d37b4
lkm5881 <lkm5881@gmail.com>
2025-09-08 20:43:26 +0900

    회원가입화면

--- lib/screens/auth/join_screen.dart
+++ lib/screens/auth/join_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:login_app/widgets/common_bottom_navigation_bar.dart';
-import 'package:login_app/widgets/custom_drawer.dart';
+import 'package:login_app/widgets/custom_button.dart';
 
 class JoinScreen extends StatefulWidget {
   const JoinScreen({super.key});
@@ -10,18 +9,129 @@ class JoinScreen extends StatefulWidget {
 }
 
 class _JoinScreenState extends State<JoinScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String? _username;            // 아이디
+  String? _password;            // 비밀번호
+  String? _confirmPassword;    // 비밀번호 확인
+  String? _name;                // 이름
+  String? _email;              // 이메일
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("회원가입"),
+        leading: SizedBox.shrink(),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.close),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: const Text("회원가입 화면"),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Center(
+                child: Image(
+                  image: AssetImage("assets/images/logo.png"),
+                  height: 100,
+                ),
+              ),
+              // 아이디
+              TextFormField(
+                validator: (value) {},
+                decoration: const InputDecoration(
+                  labelText: "아이디",
+                  hintText: "아이디를 입력하세요",
+                  prefixIcon: Icon(Icons.person_outline),
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    _username = value;
+                  });
+                },
+              ),
+              SizedBox(height: 16),
+              // 비밀번호
+              TextFormField(
+                validator: (value) {},
+                decoration: const InputDecoration(
+                  labelText: "비밀번호",
+                  hintText: "비밀번호를 입력하세요",
+                  prefixIcon: Icon(Icons.lock_outline_rounded),
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,      // 비밀번호 안보이게
+                onChanged: (value) {
+                  setState(() {
+                    _password = value;
+                  });
+                },
+              ),
+              SizedBox(height: 16),
+              // 비밀번호 확인
+              TextFormField(
+                validator: (value) {},
+                decoration: const InputDecoration(
+                  labelText: "비밀번호 확인",
+                  hintText: "비밀번호를 다시 입력하세요",
+                  prefixIcon: Icon(Icons.lock_outline_rounded),
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,      // 비밀번호 안보이게
+                onChanged: (value) {
+                  setState(() {
+                    _confirmPassword = value;
+                  });
+                },
+              ),
+              SizedBox(height: 16),
+              // 이름
+              TextFormField(
+                validator: (value) {},
+                decoration: const InputDecoration(
+                  labelText: "이름",
+                  hintText: "이름을 입력하세요",
+                  prefixIcon: Icon(Icons.person_outline),
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    _username = value;
+                  });
+                },
+              ),
+              SizedBox(height: 16),
+              // 이메일
+              TextFormField(
+                validator: (value) {},
+                decoration: const InputDecoration(
+                  labelText: "이메일",
+                  hintText: "이메일을 입력하세요",
+                  prefixIcon: Icon(Icons.email_outlined),
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    _email = value;
+                  });
+                },
+              ),
+              SizedBox(height: 16),
+            ],
+          ),
+        ),
       ),
-      endDrawer: CustomDrawer(),
-      bottomNavigationBar: CommonBottomNavigationBar(
-        currentIndex: 0
+      bottomSheet: CustomButton(
+        text: "회원가입",
+        isFullWidth: true,
+        onPressed: () {}
       ),
     );
   }
